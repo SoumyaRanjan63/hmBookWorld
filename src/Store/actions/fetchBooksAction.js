c755a428c5247e520082604463246fa119509add
SoumyaRanjan63 <130931666+SoumyaRanjan63@users.noreply.github.com>
2024-04-24 12:32:06 +0530

    Update fetchBooksAction.js

--- src/Store/actions/fetchBooksAction.js
+++ src/Store/actions/fetchBooksAction.js
@@ -3,21 +3,22 @@ import { localBooks } from "../bookSlice";
 
 export const fetchBook = () => async (dispatch) => {
   try {
-    const response = await axios.get('http://localhost:3000/books');
-    // const response = await axios.get(
-    //   "https://2bb5-125-99-156-10.ngrok-free.app/library/getbooks",
-    //   {
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       Accept: "application/json",
-    //     },
-    //   }
-    // );
+    // const response = await axios.get('http://localhost:3000/books');
+    const response = await axios.get(
+      "https://25e3-125-99-156-10.ngrok-free.app/library/books",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+      }
+    );
 
-    console.log(response);
+    console.log(response.data);
 
     dispatch(localBooks(response.data));
   } catch (error) {
     console.log(error);
   }
 };
+
